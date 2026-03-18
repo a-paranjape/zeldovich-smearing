c9e266412fe570be9cacb76914fdd4372c87166a
a-paranjape <aseem@iucaa.in>
2026-03-18 10:16:38 +0530

    ongoing update for constructing Euclid-ELG sample

--- code/pairwise_abacus.py
+++ code/pairwise_abacus.py
@@ -146,8 +146,8 @@ if __name__ == "__main__":
     
     ml = MLUtilities()
 
-    config_dict = {'DESI-LRG2': {'redshift':0.80,'Mmin':8e12,'phase':0},
-                   'Euclid-ELG':{'redshift':1.10,'Mmin':1e12,'phase':9}}
+    config_dict = {'DESI-LRG2': {'redshift':0.80,'Mmin':8e12,'phase':0,'downto':1},
+                   'Euclid-ELG':{'redshift':1.10,'Mmin':1e12,'phase':9,'downto':3}}
     
     Do_2pcf = False
     Do_Pk = True
@@ -162,7 +162,8 @@ if __name__ == "__main__":
     LOS = 2
     L_Max = 3
     
-    Down_To = 5   # default 1
+    Down_To = config_dict[Sample]['downto'] # default 1
+    
     Grid = 256    # default 256 (better than 1% convergence at k <= 0.2 h/Mpc)
     Max_File = 64 # default 64
     NProc = np.min([N_Phase,12 if Aniso else 8]) if Do_2pcf else 1 

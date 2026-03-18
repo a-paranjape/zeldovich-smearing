5ce18f09cee64936b73a57079d7c80d7d33d20d6
a-paranjape <aseem@iucaa.in>
2026-03-18 07:43:59 +0530

    ongoing update for constructing Euclid-ELG sample

--- code/pairwise_abacus.py
+++ code/pairwise_abacus.py
@@ -162,7 +162,7 @@ if __name__ == "__main__":
     LOS = 2
     L_Max = 3
     
-    Down_To = 1   # default 1
+    Down_To = 3   # default 1
     Grid = 256    # default 256 (better than 1% convergence at k <= 0.2 h/Mpc)
     Max_File = 64 # default 64
     NProc = np.min([N_Phase,12 if Aniso else 8]) if Do_2pcf else 1 

5836342d74b3c66233fa750cadc52b797f9e2324
a-paranjape <aseem@iucaa.in>
2026-03-18 13:01:03 +0530

    ongoing update for constructing Euclid-ELG sample

--- code/pairwise_abacus.py
+++ code/pairwise_abacus.py
@@ -154,8 +154,8 @@ if __name__ == "__main__":
     config_dict = {'DESI-LRG2': {'redshift':0.80,'Mmin':8e12,'phase':0,'downto':1},
                    'Euclid-ELG':{'redshift':1.10,'Mmin':1e12,'phase':0,'downto':1}}
     
-    Do_2pcf = True
-    Do_Pk = False
+    Do_2pcf = False
+    Do_Pk = True
 
     # number of boxes to analyse, should be > Ref_Phase, or equal to N in case of '-xN' filename
     N_Phase = 1

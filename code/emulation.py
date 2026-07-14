bug fix in sigv normalization for neutrino cosmologies

code/emulation.py
@@ -450,7 +450,9 @@ class AgnosticEmulator(Utilities,MLUtilities):
         sigv2 = np.trapezoid(Dlin_int/self.ktab_int**2,dx=self.dlnk_int)/3.0
         sigv2_frac = np.trapezoid(Dlin_int[self.cond_k]/self.ktab_int[self.cond_k]**2,dx=self.dlnk_int)/3.0
 
-        sigv = growth*np.sqrt(sigv2)
+        sigv = np.sqrt(sigv2)
+        if self.cosmo not in self.neutrino_cosmologies:
+            sigv *= growth
         fv = sigv2_frac/(sigv2 + co.TINY)
 
         # distances (nominally in Mpc/h)